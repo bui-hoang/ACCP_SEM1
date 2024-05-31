488824c5cd94ec9302062207a5498065f37ba4e9
bui-hoang <gangtergamingx@gmail.com>
2024-05-31 08:10:32 +0700

    java
    java

--- c02/js/boolean-variable.js
+++ c02/js/boolean-variable.js
@@ -7,4 +7,4 @@ var elStock = document.getElementById('stock');
 elStock.className = inStock;
 
 var elShip = document.getElementById('shipping');
-elShip.className = shipping;
+elShip.className = shipping; 
---
+++ javaScript-04/js/date-object.js
@@ -0,0 +1,7 @@
+var today = new Date();
+
+var year = today.getFullYear();
+
+var el = document.getElementById('footer');
+
+el.innerHTML = '<p>CopyRight &coppy;' + year + '</p>';
